51bc482279ce1fbd144d18ad15dbf50b44725a8d
khokan_gorain <khokangorain24hour@gmail.com>
2022-11-23 20:53:07 +0530

    This is initial commit

---
+++ app/src/main/java/khokan_gorain/newproject/MainActivity.java
@@ -0,0 +1,14 @@
+package khokan_gorain.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
